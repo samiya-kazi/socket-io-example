b9f4b0203a6f3db266bd8bb2c60b797a7e556ac8
Samiya Kazi <samiya.kazi09@gmail.com>
2024-07-16 10:49:14 +0600

    feat: created base server with sokcet.io

---
+++ server/src/index.ts
@@ -0,0 +1,51 @@
+import express, { Request, Response } from 'express';
+import cors, { CorsOptions } from "cors";
+import http from "http";
+import { Server } from "socket.io";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+const corsConfig : CorsOptions = { origin: "*" };
+
+app.use(cors(corsConfig));
+app.use(express.json());
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.json('Hello world!');
+})
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`🚀 Server is running on port ${PORT}.`));
+
+const io = new Server(server, {
+  cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+
+  socket.emit("me", socket.id);
+
+  socket.on("join-room", (data: { roomId: string }) => {
+    socket.join(data.roomId);
+    socket.emit('join-room-success', { roomId: data.roomId });
+  });
+
+  socket.on("leave-room", (data: { roomId: string }) => {
+    socket.leave(data.roomId);
+    socket.emit('leave-room-success', { roomId: data.roomId });
+  });
+
+  socket.on("send-message", (data: { roomId: string, message: string }) => {
+    socket.to(data.roomId).emit('new-message', data);
+  });
+
+});
