55676b6c994d661ce6ef3ccdf1f08075fb448194
Samiya Kazi <samiya.kazi09@gmail.com>
2024-07-17 20:39:22 +0600

    feat: added comments to simple server and client

--- simple/client/src/App.tsx
+++ simple/client/src/App.tsx
@@ -9,11 +9,17 @@ const URL = import.meta.env.SERVER_URL ?? 'http://localhost:3000';
 function App() {
   const [content, setContent] = useState<IQuote>({ quote: "", author: "" });
   
+  // When this component mounts, we will establish a socket connection to the server
   useEffect(() => {
     const socket = io(URL);
 
+    // Listen for the "connected" event and log the received data
+    socket.on("connected", (data: string) => console.log(data));
+
+    // Listen for the "new-quote" event and update the content state
     socket.on('new-quote', (data: IQuote) => setContent(data));
 
+    // Cleanup function to disconnect the socket when the component unmounts
     return () => {
       socket.disconnect();
     }
--- simple/server/src/index.ts
+++ simple/server/src/index.ts
@@ -15,34 +15,38 @@ app.use(cors(corsConfig));
 app.use(express.json());
 
 
-app.get('/', (req: Request, res: Response) => {
-  res.json('Hello world!');
-})
-
+// Create an HTTP server instance using the Express application.
 const server = http.createServer(app);
 
+// Start the server and listen on the specified port
 server.listen(PORT, () => console.log(`🚀 Server is running on port ${PORT}.`));
 
-
+// Create a new socket.io server instance with CORS configuration
 const io = new Server(server, {
   cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-      credentials: true,
+      origin: "*",          // Allow all origins
+      methods: ["GET", "POST"], // Allow GET and POST methods
+      credentials: true,    // Allow credentials
   },
 });
 
 
+// Whenever a client connects to the server, 
+// the provided callback function is executed.
 io.on("connection", (socket) => {
 
+  // socket.emit is used to send an event to the client.
   socket.emit("connected", socket.id);
   socket.emit('new-quote', getRandomQuote());
 
+  // Every 5 seconds, a new quote is sent to the client.
   setInterval(() => {
     socket.emit('new-quote', getRandomQuote());
   }, 3000);
 });
 
+
+// Function to get a random quote from the list of quotes
 function getRandomQuote () {
   const index = Math.floor(Math.random() * inspirationalQuotes.length);
   return inspirationalQuotes[index];
